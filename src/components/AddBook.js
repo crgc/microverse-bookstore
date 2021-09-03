38313e4782bc0ae350927d9649b91a961829f973
Carlos Gonzalez <carlosfwrd@gmail.com>
2021-09-03 07:22:06 -0400

    Add addBook basic structure

--- src/components/AddBook.js
+++ src/components/AddBook.js
@@ -1,29 +1,46 @@
-const AddBook = () => (
-  <div className="add-book-section">
-    <div className="add-book-header">ADD NEW BOOK</div>
-    <form className="add-book-form">
-      <input
-        type="text"
-        className="add-book-title"
-        placeholder="Title"
-        name="title"
-      />
-      <input
-        type="text"
-        className="add-book-author"
-        placeholder="Author"
-        name="author"
-      />
-      <div className="add-book-wrapper">
-        <select name="category" className="add-book-category">
-          <option value="category">Category</option>
-          <option value="fantasy">Fantasy</option>
-          <option value="fantasy">Journalism</option>
-        </select>
-        <button type="button" className="btn add-book-btn">ADD BOOK</button>
-      </div>
-    </form>
-  </div>
-);
+import { useDispatch } from 'react-redux';
+import {addBook, removeBook} from './redux/books/books';
+
+const AddBook = () => {
+  const dispatch = useDispatch();
+
+  const submitBookToStore = () => {
+    const newBook = {
+      id,
+      title,
+      author
+    }
+      
+    dispatch(addBook(newBook));
+  }
+
+  return (
+    <div className="add-book-section">
+      <div className="add-book-header">ADD NEW BOOK</div>
+      <form className="add-book-form">
+        <input
+          type="text"
+          className="add-book-title"
+          placeholder="Title"
+          name="title"
+        />
+        <input
+          type="text"
+          className="add-book-author"
+          placeholder="Author"
+          name="author"
+        />
+        <div className="add-book-wrapper">
+          <select name="category" className="add-book-category">
+            <option value="category">Category</option>
+            <option value="fantasy">Fantasy</option>
+            <option value="fantasy">Journalism</option>
+          </select>
+          <button type="button" onClick={submitBookToStore} className="btn add-book-btn">ADD BOOK</button>
+        </div>
+      </form>
+    </div>
+  );
+};
 
 export default AddBook;
